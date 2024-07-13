84475fa550c39da4e58c64c1c72aad0418ae03c5
F4nnes <tonatiuh_06flores@live.com>
2024-07-13 00:13:33 -0600

    "elaboracion de banner y su integracion con body, pendiente hacer etiquetas"

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,27 @@
+
+
+import CardBanner from "components/CardBanner";
+import { styled } from "styled-components";
+
+const FigureEstilizada = styled.figure`
+  background-image: ${props => `url(${props.$backgroundImage})`};
+  flex-grow: 1;
+  background-repeat: no-repeat;
+  display: flex;
+  align-items: center;
+  min-height: 832px;
+  margin: 0;
+  max-width: 100%;
+  background-size: cover;
+`;
+
+
+const Banner = ({ backgroundImage }) => {
+  return (
+    <FigureEstilizada $backgroundImage={backgroundImage}>
+      <CardBanner />
+    </FigureEstilizada>
+  );
+};
+
+export default Banner;
--- src/pages/inicio/index.js
+++ src/pages/inicio/index.js
@@ -1,12 +1,14 @@
+import Banner from "components/Banner/Banner"
 import Contenedor from "components/Contenedor"
 import Footer from "components/footer"
 import Header from "components/header"
-
+import banner from "./Banner.png"
 
 function Inicio(){
     return(
         <>
         <Header/>
+        <Banner backgroundImage={banner}/>
         <Contenedor/>
         <Footer/>
         </>
